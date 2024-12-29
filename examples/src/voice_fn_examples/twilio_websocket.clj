97c3cdd5eadcc5b64d62c9db7004f4b1e2e0ba40
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-12-29 12:27:39 +0200

    Started with examples

---
+++ examples/src/voice_fn_examples/twilio_websocket.clj
@@ -0,0 +1,93 @@
+(ns voice-fn-examples.twilio-websocket
+  (:require
+   [clojure.data.xml :as xml]
+   [muuntaja.core :as m]
+   [reitit.core]
+   [reitit.dev.pretty :as pretty]
+   [reitit.ring :as ring]
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.exception :as exception]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.parameters :as parameters]
+   [ring.adapter.jetty :as jetty]
+   [ring.websocket :as ws]))
+
+(defn emit-xml-str
+  [xml-data]
+  (-> xml-data
+      (xml/sexp-as-element)
+      (xml/emit-str)))
+
+(defn header
+  [req header-name]
+  (get (:headers req) header-name))
+
+(defn host
+  [req]
+  (header req "host"))
+
+(defn twilio-inbound-handler
+  "Handler to direct the incoming call to stream audio to our websocket handler"
+  [req]
+  (let [h (host req)
+        ws-url (str "wss://" h "/ws")]
+    ;; https://www.twilio.com/docs/voice/twiml/connect
+    (emit-xml-str [:Response
+                   [:Connect
+                    [:Stream {:url ws-url}]]])))
+
+;; Using ring websocket protocols to setup a websocket server
+(defn twilio-ws-handler
+  [req]
+  (assert (ws/upgrade-request? req))
+  (let [ws-handler
+        (fn [upgrade-request]
+          (let [provided-subprotocols (:websocket-subprotocols upgrade-request)]
+            {::ws/listener
+             {:on-open (fn on-connect [_]
+                         (prn ::ws-connected))
+              :on-message (fn on-text [_ws payload])
+              :on-close (fn on-close [_ws status-code reason])
+              :on-ping (fn on-ping [ws payload]
+                         (ws/send ws payload))}
+             ::ws/protocol (first provided-subprotocols)}))]
+    (ws-handler req)))
+
+(def routes
+  [["/inbound-call" {:summary "Webhook where a call is made"
+                     :get {:handler (fn [req])}}]
+   ["/ws" {:summary "Websocket endpoint to receive a twilio call"
+           :get {:handler twilio-ws-handler}}]])
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      routes
+      {:exception pretty/exception
+       :data {:muuntaja m/instance
+              :middleware [;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware]}})
+    (ring/create-default-handler)))
+
+(defn start [& {:keys [port] :or {port 3000}}]
+  (jetty/run-jetty #'app {:port port, :join? false})
+  (println (str "server running in port" port)))
+
+(comment
+
+  (def server (start :port 3001))
+  (.stop server)
+
+  ,)
