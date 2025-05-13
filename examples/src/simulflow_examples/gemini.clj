e004827900ea59fa899fe11d51a2a1d1e23ac45d
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-05-13 08:11:20 +0300

    Update variable names for gemini

--- examples/src/simulflow_examples/gemini.clj
+++ examples/src/simulflow_examples/gemini.clj
@@ -8,26 +8,27 @@
    [simulflow.secrets :refer [secret]]
    [taoensso.telemere :as t]))
 
-(def local-flow-groq (local/make-local-flow {:extra-procs {:llm {:proc google/google-llm-process
-                                                                 :args {:llm/model :gemini-2.0-flash
-                                                                        :google/api-key (secret [:google :api-key])}}}
-                                             :llm-context {:messages
-                                                           [{:role "system"
-                                                             :content "You are a voice agent operating via phone. Be
+(def local-flow-gemini
+  (local/make-local-flow {:extra-procs {:llm {:proc google/google-llm-process
+                                              :args {:llm/model :gemini-2.0-flash
+                                                     :google/api-key (secret [:google :api-key])}}}
+                          :llm-context {:messages
+                                        [{:role "system"
+                                          :content "You are a voice agent operating via phone. Be
                        concise in your answers. The input you receive comes from a
                        speech-to-text (transcription) system that isn't always
                        efficient and may send unclear text. Ask for
                        clarification when you're unsure what the person said."}]
-                                                           :tools []}}))
+                                        :tools []}}))
 
 (defonce flow-started? (atom false))
 
 (comment
   ;; Start local ai flow - starts paused
-  (let [{:keys [report-chan error-chan]} (flow/start local-flow-groq)]
+  (let [{:keys [report-chan error-chan]} (flow/start local-flow-gemini)]
     (reset! flow-started? true)
     ;; Resume local ai -> you can now speak with the AI
-    (flow/resume local-flow-groq)
+    (flow/resume local-flow-gemini)
     (vthread-loop []
       (when @flow-started?
         (when-let [[msg c] (a/alts!! [report-chan error-chan])]
@@ -37,6 +38,6 @@
 
   ;; Stop the conversation
   (do
-    (flow/stop local-flow-groq)
+    (flow/stop local-flow-gemini)
     (reset! flow-started? false))
   ,)
