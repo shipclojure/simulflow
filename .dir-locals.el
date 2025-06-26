98f43e72105c23b87c56a3167a73304f851e78b9
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-06-26 17:03:53 +0300

    Add development conveniences

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((cider-preferred-build-tool       . clojure-cli)
+         (cider-clojure-cli-aliases        . ":dev:test:with-examples:clj-reload")
+         (cider-clojure-cli-parameters     . "--port 8000"))))
---
+++ dev/simulflow/dev.clj
@@ -0,0 +1,46 @@
+(ns simulflow.dev
+  (:require [clj-reload.core :as reload]
+            [clojure.java.io :as io]
+            [malli.dev :as malli-dev]))
+
+(defmacro jit [sym]
+  `(requiring-resolve '~sym))
+
+(defonce initiated-clj-reload? (atom false))
+
+(defn reset
+  []
+  (when-not @initiated-clj-reload?
+    (reload/init {:dirs ["src" "dev" "test" "../examples/src"]})
+    (reset! initiated-clj-reload? true))
+  (reload/reload))
+
+(defn export-types []
+  ;; collect schemas and start instrumentation
+  ((jit malli-dev/start!))
+
+  ;; create export file
+  (def export-file
+    (io/file "resources/clj-kondo.exports/com.shipclojure/simulflow_types/config.edn"))
+
+  ;; make parents if not exist
+  (io/make-parents export-file)
+
+  ;; copy the configs
+  (io/copy
+   (io/file ".clj-kondo/metosin/malli-types-clj/config.edn")
+   export-file)
+
+  ;; clear the cache and stop instrumentation
+  ((jit malli-dev/stop!)))
+
+(comment ;; s-:
+
+  (reset)
+
+  )
+
+
+(comment
+  (export-types)
+  ,)
