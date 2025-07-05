cc569cbee33635539ca7ff04beb021a295ff7580
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-05 07:36:22 +0300

    Google LLM transition function return state always

--- src/simulflow/processors/google.clj
+++ src/simulflow/processors/google.clj
@@ -31,25 +31,25 @@
 
 (def model-schema
   (schema/flex-enum
-    {:description "Google llm model identifier"
-     :error/message "Must be a valid Google LLM model. Try gemini-2.0-flash"}
-    ["gemini-embedding-exp" "gemini-2.0-flash-thinking-exp" "learnlm-1.5-pro-experimental" "gemini-1.5-pro" "gemini-2.0-flash-lite-001" "gemini-1.5-flash"
-     "gemini-2.0-flash-lite-preview" "chat-bison-001" "gemini-exp-1206" "gemini-1.5-pro-002" "text-bison-001" "gemini-2.0-flash-lite-preview-02-05"
-     "gemini-1.5-flash-8b-latest" "embedding-001" "gemini-2.0-pro-exp" "gemini-1.5-flash-8b-001" "gemini-1.0-pro-vision-latest" "gemini-2.5-pro-preview-03-25"
-     "learnlm-2.0-flash-experimental" "gemini-1.5-flash-002" "gemma-3-27b-it" "text-embedding-004" "gemini-1.5-flash-001-tuning" "gemini-2.0-flash-lite"
-     "gemini-2.5-flash-preview-04-17-thinking" "gemini-1.5-pro-001" "gemini-pro-vision" "gemini-1.5-flash-8b-exp-0924" "gemini-2.0-flash-live-001" "aqa"
-     "gemini-1.5-flash-8b" "gemini-2.0-flash-thinking-exp-1219" "gemini-1.5-flash-latest" "gemini-1.5-pro-latest" "gemma-3-4b-it" "embedding-gecko-001"
-     "gemini-2.0-flash-thinking-exp-01-21" "gemini-2.0-pro-exp-02-05" "veo-2.0-generate-001" "gemini-embedding-exp-03-07" "gemini-2.5-pro-exp-03-25" "gemini-1.5-flash-8b-exp-0827"
-     "gemma-3-12b-it" "gemini-2.0-flash" "gemini-2.5-flash-preview-04-17" "gemini-2.0-flash-001" "gemini-1.5-flash-001" "gemma-3-1b-it" "gemini-2.5-pro-preview-05-06"
-     "imagen-3.0-generate-002" "gemini-2.0-flash-exp"]))
+   {:description "Google llm model identifier"
+    :error/message "Must be a valid Google LLM model. Try gemini-2.0-flash"}
+   ["gemini-embedding-exp" "gemini-2.0-flash-thinking-exp" "learnlm-1.5-pro-experimental" "gemini-1.5-pro" "gemini-2.0-flash-lite-001" "gemini-1.5-flash"
+    "gemini-2.0-flash-lite-preview" "chat-bison-001" "gemini-exp-1206" "gemini-1.5-pro-002" "text-bison-001" "gemini-2.0-flash-lite-preview-02-05"
+    "gemini-1.5-flash-8b-latest" "embedding-001" "gemini-2.0-pro-exp" "gemini-1.5-flash-8b-001" "gemini-1.0-pro-vision-latest" "gemini-2.5-pro-preview-03-25"
+    "learnlm-2.0-flash-experimental" "gemini-1.5-flash-002" "gemma-3-27b-it" "text-embedding-004" "gemini-1.5-flash-001-tuning" "gemini-2.0-flash-lite"
+    "gemini-2.5-flash-preview-04-17-thinking" "gemini-1.5-pro-001" "gemini-pro-vision" "gemini-1.5-flash-8b-exp-0924" "gemini-2.0-flash-live-001" "aqa"
+    "gemini-1.5-flash-8b" "gemini-2.0-flash-thinking-exp-1219" "gemini-1.5-flash-latest" "gemini-1.5-pro-latest" "gemma-3-4b-it" "embedding-gecko-001"
+    "gemini-2.0-flash-thinking-exp-01-21" "gemini-2.0-pro-exp-02-05" "veo-2.0-generate-001" "gemini-embedding-exp-03-07" "gemini-2.5-pro-exp-03-25" "gemini-1.5-flash-8b-exp-0827"
+    "gemma-3-12b-it" "gemini-2.0-flash" "gemini-2.5-flash-preview-04-17" "gemini-2.0-flash-001" "gemini-1.5-flash-001" "gemma-3-1b-it" "gemini-2.5-pro-preview-05-06"
+    "imagen-3.0-generate-002" "gemini-2.0-flash-exp"]))
 
-(def GoogleLLMConfigSchema  [:map
-                             {:description "Google LLM configuration"}
-                             [:llm/model {:default :gemini-2.0-flash} model-schema]
-                             [:google/api-key  [:string
-                                                {:description "Google API key"
-                                                 :secret true
-                                                 :error/message "Invalid Google"}]]])
+(def GoogleLLMConfigSchema
+  [:map
+   {:description "Google LLM configuration"}
+   [:llm/model {:default :gemini-2.0-flash} model-schema]
+   [:google/api-key [:string
+                     {:description "Google API key"
+                      :error/message "Invalid Google Api Key provided"}]]])
 
 (comment
 
@@ -96,10 +96,11 @@
          (recur)))
      {::flow/in-ports {:llm-read llm-read}
       ::flow/out-ports {:llm-write llm-write}}))
-  ([{::flow/keys [in-ports out-ports]} transition]
+  ([{::flow/keys [in-ports out-ports] :as state} transition]
    (when (= transition ::flow/stop)
      (doseq [port (concat (vals in-ports) (vals out-ports))]
-       (a/close! port))))
+       (a/close! port)))
+   state)
   ([state in msg]
    (if (= in :llm-read)
      [state {:out [msg]}]
